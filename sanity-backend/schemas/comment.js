e573d78b1124d7488999bc8713a4303050fc7293
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-11-20 18:20:36 -0500

    uploaded schema files

---
+++ sanity-backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    {
+      name: "postedBy",
+      tiitle: "Posted By",
+      type: "postedBy",
+    },
+    {
+      name: "comment",
+      title: "Comment",
+      type: "string",
+    },
+  ],
+};
---
+++ sanity-backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: "user",
+  title: "User",
+  type: "document",
+  fields: [
+    {
+      name: "userName",
+      tiitle: "User Name",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "string",
+    },
+  ],
+};
