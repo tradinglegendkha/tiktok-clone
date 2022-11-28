89e4b7b3434857d88e275b47fad98444b73d0d81
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-11-27 21:54:50 -0500

    changed logo, might replace in future

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,15 +7,16 @@ import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 import logo from "../utils/logo.png";
+import tlogo from "../utils/tlogo.png";
 
 const Navbar = () => {
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
-        <div className="w-[100px] md:w-[129px] md:h-[30px] h-[38px]">
+        <div className="w-[100px] md:w-[129px]">
           <Image
             className="cursor-pointer"
-            src={logo}
+            src={tlogo}
             alt="logo"
             layout="responsive"
           />
