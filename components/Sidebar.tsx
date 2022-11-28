3d695261b79a288d1fceaef6567cf32c868c8250
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-11-27 21:55:28 -0500

    added sidebar, unfinished

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import GoogleLogin from "react-google-login";
+import { AiFillHome, AiOutlineMenu, AiOutlineTable } from "react-icons/ai";
+import { ImCancelCircle } from "react-icons/im";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [showSidebar, setShowSidebar] = useState(true);
+  const userProfile = false;
+  const normalLink =
+    "flex items-center gap-3 hober:bg-primary p-3 justify-center x1:justify-start cursor-pointer font-semibold text-[#F51997] rounded";
+
+  return (
+    <div>
+      <div
+        className="block x1:hidden m-2 m1-4 mt-3 text-x1"
+        onClick={() => setShowSidebar((prev) => !prev)}
+      >
+        {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+      </div>
+      {showSidebar && (
+        <div
+          className="x1:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 
+        border-gray-10 x1:border-0 p-3"
+        >
+          <div
+            className="x1:border-b-2
+          border-gray-200 x1:pb-4"
+          >
+            <Link href="/">
+              <div className={normalLink}>
+                <p className="text-2x1">
+                  <AiFillHome />
+                </p>
+                <span className="text-x1 hidden x1:block">For you</span>
+              </div>
+            </Link>
+          </div>
+          {!userProfile && (
+            <div className="px-2 py-4 hidden x1:block">
+              <p className="text-gray-400">
+                Log in to like and comment on vidoes
+              </p>
+              <div className="pr-4">
+                <GoogleLogin
+                  clientId=""
+                  render={(renderProps) => (
+                    <button
+                      className="bg-white text-lg text-[#F51997]"
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      Log in
+                    </button>
+                  )}
+                  onSuccess={() => {}}
+                  onFailure={() => {}}
+                  cookiePolicy="single_host_origin"
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
