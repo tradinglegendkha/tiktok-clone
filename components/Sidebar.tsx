7ab06e5442986abd193ffa7ebd8a5882ec223d2d
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-11-29 19:49:12 -0500

    fixed issue

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,36 +13,36 @@ const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true);
   const userProfile = false;
   const normalLink =
-    "flex items-center gap-3 hover:bg-primary p-3 justify-center x1:justify-start cursor-pointer font-semibold text-[#F51997] rounded";
+    "flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded";
 
   return (
     <div>
       <div
-        className="block x1:hidden m-2 m1-4 mt-3 text-x1"
+        className="block xl:hidden m-2 m1-4 mt-3 text-xl"
         onClick={() => setShowSidebar((prev) => !prev)}
       >
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
       {showSidebar && (
         <div
-          className="x1:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 
-        border-gray-10 x1:border-0 p-3"
+          className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 
+        border-gray-10 xl:border-0 p-3"
         >
           <div
-            className="x1:border-b-2
-          border-gray-200 x1:pb-4"
+            className="xl:border-b-2
+          border-gray-200 xl:pb-4"
           >
             <Link href="/">
               <div className={normalLink}>
-                <p className="text-2x1">
+                <p className="text-2xl">
                   <AiFillHome />
                 </p>
-                <span className="text-x1 hidden x1:block">For you</span>
+                <span className="text-xl hidden xl:block">For you</span>
               </div>
             </Link>
           </div>
           {!userProfile && (
-            <div className="px-2 py-4 hidden x1:block">
+            <div className="px-2 py-4 hidden xl:block">
               <p className="text-gray-400">
                 Log in to like and comment on vidoes
               </p>
@@ -53,7 +53,7 @@ const Sidebar = () => {
                     <button
                       className="bg-white text-lg text-[#F51997] border-[1px] 
                       border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 
-                      hober:text-white hober:bg-[#F51997]"
+                      hover:text-white hover:bg-[#F51997]"
                       onClick={renderProps.onClick}
                       disabled={renderProps.disabled}
                     >
