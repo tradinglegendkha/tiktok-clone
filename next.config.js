bb8db8b78b4c3c2d6f30bac276e9d4e0cfb1cd04
Buppi Khan <khanbuppi@gmail.com>
2022-12-04 13:34:32 -0500

    profile and name shows whenever someone uploads

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["yt3.ggpht.com", "www.nationalreview.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,33 @@
 import type { NextPage } from "next";
 import axios from "axios";
+import { Video } from "../types";
+import VideoCard from "../components/VideoCard";
+import NoResults from "../components/NoResults";
 
-const Home: NextPage = () => {
-  return <div>index</div>;
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"No Videoes"} />
+      )}
+    </div>
+  );
 };
 
 export const getServerSideProps = async () => {
-  const response = await axios.get(`http://localhost:3000/api/post`);
-  console.log(response.data.name);
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
 
   return {
-    props: {},
+    props: {
+      videos: data,
+    },
   };
 };
 
